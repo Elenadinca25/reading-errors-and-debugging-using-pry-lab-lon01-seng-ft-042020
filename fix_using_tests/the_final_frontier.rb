a7c040f8a5fec357ec6ee2ba2adae359f7b016ba
Elena Dinca <elenadinca25@gmail.com>
2020-04-02 21:57:07 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -17,7 +17,7 @@ def greet_crew(crew)
   crew.map { |crew_member| "Hello #{crew_member}." }
 end
 
-def engage(state_log, star_date)
+def engage(state_log, crew_member)
   state_log(date)
   date = generate_star_date
  return state_log(crew)
