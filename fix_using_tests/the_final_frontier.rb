b862f6b40404930b546accebd7c7d20f60b481fa
Elena Dinca <elenadinca25@gmail.com>
2020-04-02 21:15:12 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -9,11 +9,10 @@ def state_log(star_date)
  "Captain's Log, star date #{star_date}."
 end
 
-def crew
-  ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
 end
 
 def greet_crew(crew)
+  crew = ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
   crew.each { |crew_member| "Hello #{crew_member}."}
 end
 
